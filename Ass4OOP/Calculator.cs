f458a3fb5b206ca0e920473a95fc1c491e4e9ed2
Fadynour <fadynour194@gmail.com>
2025-08-07 18:28:44 +0300

    Q1

---
+++ Ass4OOP/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass4OOP
+{
+    internal class Calculator
+    {
+        #region part 1 Q1 
+        public static int Add(int x, int y) {  return x + y; }
+        public static int Add(int x, int y,int z) {  return x + y+z; }
+        public static double Add(double x, double y) { return x + y; }
+        #endregion
+    }
+}
