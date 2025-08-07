eb0b4ebdf93fdee243605db19d76672f4a33d05b
Fadynour <fadynour194@gmail.com>
2025-08-07 18:50:51 +0300

    Q4

--- Ass4OOP/Calculator.cs
+++ Ass4OOP/Calculator.cs
@@ -56,4 +56,22 @@ namespace Ass4OOP
         }
     }
     #endregion
+    #region Q4
+    class Employee
+    {
+         public virtual void Work()
+         {
+            Console.WriteLine("Employee is  working");
+         }
+    }
+    class Manager :Employee
+    {
+        public override void Work()
+        {
+            base.Work();
+            Console.WriteLine("Manager is managing");
+        }
+
+    }
+    #endregion
 }
